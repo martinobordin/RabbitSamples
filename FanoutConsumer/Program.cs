eb40974b7732c8dd201c6daf28e43122e93202a6
Martino Bordin <info@martinobordin.it>
2023-03-10 18:04:32 +0100

    Add project files.

---
+++ FanoutConsumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var exchangeName = "ex.fanout";
+var queue1Name = "my.queue1";
+var queue2Name = "my.queue2";
+var routingKey = string.Empty;
+
+var connectionFactory = new ConnectionFactory
+{
+    HostName = "localhost",
+    VirtualHost = "/",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+
+using var connection = connectionFactory.CreateConnection();
+using var channel = connection.CreateModel();
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine($"Received: {message}");
+
+    // Manual Ack
+    channel.BasicAck(e.DeliveryTag, false);
+
+    //channel.BasicNack(e.DeliveryTag, false, true);
+};
+
+channel.BasicConsume(queue1Name, autoAck: false, consumer);
+Console.ReadKey(true);
---
+++ FanoutPublisher/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var exchangeName = "ex.fanout";
+var queue1Name = "my.queue1";
+var queue2Name = "my.queue2";
+var routingKey = string.Empty;
+var counter = 0;
+
+var connectionFactory = new ConnectionFactory
+{
+    HostName = "localhost",
+    VirtualHost = "/",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+
+using var connection = connectionFactory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchangeName, "fanout", true, false);
+channel.QueueDeclare(queue1Name, true, false, false);
+channel.QueueDeclare(queue2Name, true, false, false);
+
+channel.QueueBind(queue1Name, exchangeName, routingKey);
+channel.QueueBind(queue2Name, exchangeName, routingKey);
+
+while (true)
+{
+    Console.Write("Press any key to publish a message, or 'X' to quit, or ");
+    Console.WriteLine("CTRL+C to interrupt the read operation:");
+
+    // Start a console read operation. Do not display the input.
+    var cki = Console.ReadKey(true);
+
+    // Announce the name of the key that was pressed .
+    Console.WriteLine($"  Key pressed: {cki.Key}\n");
+
+    // Exit if the user pressed the 'X' key.
+    if (cki.Key == ConsoleKey.X) break;
+
+    var message = $"Hello, it's {counter++}";
+    channel.BasicPublish(exchangeName, routingKey, null, Encoding.UTF8.GetBytes(message));
+    Console.WriteLine($"Published: {message}");
+}
+
+//channel.QueueDelete(queue1Name);
+//channel.QueueDelete(queue2Name);
+//channel.ExchangeDelete(exchangeName);
